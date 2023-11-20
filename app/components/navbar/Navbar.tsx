48705062028d5945c0cb22c8b166c7c43b47269d
donellnjagix <74359807+donellnjagix@users.noreply.github.com>
2023-11-21 02:33:19 +0300

    Desighn

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -10,28 +10,28 @@ const Navbar: React.FC = () => {
 
   return (
     <div className="navbar-container" style={{ display: 'flex', justifyContent: 'center', backgroundColor: '#3C8474', color: '#ECF4FC' }}>
-      <div className="navbar" style={{ display: 'flex', alignItems: 'center', maxWidth: '600px', width: '100%' }}>
-        <button className="navbar-toggle" onClick={handleNavbarCollapse} style={{ borderColor: '#5C9C8C', color: '#ECF4FC' }}>
-          <span className="navbar-toggle-icon"></span>
+      <div className="navbar" style={{ display: 'flex', alignItems: 'center', maxWidth: '800px', width: '100%' }}>
+        <button className="navbar-toggle" onClick={handleNavbarCollapse} style={{ borderColor: '#5C9C8C', color: '#ECF4FC', fontSize: '1.5rem' }}>
+          <span className="navbar-toggle-icon">&#9776;</span>
         </button>
-        <div className={`navbar-collapse ${isNavbarCollapsed ? 'collapsed' : ''}`} style={{ backgroundColor: '#ACCCC4', marginLeft: '10px' }}>
-          {/* Removed the profile picture section */}
-          <p className="navbar-user-name" style={{ color: '#295B50', marginLeft: '5px' }}>
-            John Doe
-          </p>
+        <div className={`navbar-collapse ${isNavbarCollapsed ? 'collapsed' : ''}`} style={{ backgroundColor: '#ACCCC4', marginLeft: '10px', padding: '10px' }}>
           <Link href="/home">
-            <span style={{ color: '#5C9C8C', margin: '5px' }}>Home</span>
+            <span style={{ color: '#5C9C8C', margin: '5px', fontSize: '1.2rem' }}>Home</span>
           </Link>
           <Link href="/account-profile">
-            <span style={{ color: '#5C9C8C', margin: '5px' }}>Account Profile</span>
+            <span style={{ color: '#5C9C8C', margin: '5px', fontSize: '1.2rem' }}>Account Profile</span>
           </Link>
           <Link href="/bank-balance">
-            <span style={{ color: '#5C9C8C', margin: '5px' }}>Bank Balance</span>
+            <span style={{ color: '#5C9C8C', margin: '5px', fontSize: '1.2rem' }}>Bank Balance</span>
           </Link>
           <Link href="/change-password">
-            <span style={{ color: '#5C9C8C', margin: '5px' }}>Change Password</span>
+            <span style={{ color: '#5C9C8C', margin: '5px', fontSize: '1.2rem' }}>Change Password</span>
           </Link>
         </div>
+        {/* Username moved to the right */}
+        <p className="navbar-user-name" style={{ color: '#295B50', marginLeft: 'auto', marginRight: '10px', fontSize: '1.2rem' }}>
+          John Doe
+        </p>
       </div>
     </div>
   );

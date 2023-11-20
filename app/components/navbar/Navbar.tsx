c149f3b89f9519c61e62066531201c9b577a7ada
donellnjagix <74359807+donellnjagix@users.noreply.github.com>
2023-11-20 20:00:42 +0300

    Created a new navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,39 +1,47 @@
 import Link from 'next/link';
 import { useState } from 'react';
 
-
 const Navbar: React.FC = () => {
- const [isNavbarCollapsed, setIsNavbarCollapsed] = useState(true);
+  const [isNavbarCollapsed, setIsNavbarCollapsed] = useState(true);
 
- const handleNavbarCollapse = () => {
+  const handleNavbarCollapse = () => {
     setIsNavbarCollapsed(!isNavbarCollapsed);
- };
+  };
 
- return (
-    <div className="navbar">
-      <button className="navbar-toggle" onClick={handleNavbarCollapse}>
-        <span className="navbar-toggle-icon"></span>
-      </button>
-      <div className={`navbar-collapse ${isNavbarCollapsed ? 'collapsed' : ''}`}>
-        <div className="navbar-user">
-          <img className="navbar-user-image" src="public\vercel.svg" alt="Profile Picture" />
-          <p className="navbar-user-name">John Doe</p>
+  return (
+    <div className="navbar-container" style={{ display: 'flex', justifyContent: 'center', backgroundColor: '#3C8474', color: '#A3D5CA' }}>
+      <div className="navbar" style={{ display: 'flex', alignItems: 'center', maxWidth: '600px', width: '100%' }}>
+        <button className="navbar-toggle" onClick={handleNavbarCollapse} style={{ borderColor: '#5C9C8C' }}>
+          <span className="navbar-toggle-icon"></span>
+        </button>
+        <div className={`navbar-collapse ${isNavbarCollapsed ? 'collapsed' : ''}`} style={{ backgroundColor: '#ACCCC4', marginLeft: '10px' }}>
+          <div className="navbar-user">
+            <img
+              className="navbar-user-image"
+              src="public\vercel.svg"
+              alt="Profile Picture"
+              style={{ borderRadius: '50%', border: '2px solid #40847C' }}
+            />
+            <p className="navbar-user-name" style={{ color: '#295B50', marginLeft: '5px' }}>
+              John Doe
+            </p>
+          </div>
+          <Link href="/home">
+            <span style={{ color: '#5C9C8C', margin: '5px' }}>Home</span>
+          </Link>
+          <Link href="/account-profile">
+            <span style={{ color: '#5C9C8C', margin: '5px' }}>Account Profile</span>
+          </Link>
+          <Link href="/bank-balance">
+            <span style={{ color: '#5C9C8C', margin: '5px' }}>Bank Balance</span>
+          </Link>
+          <Link href="/change-password">
+            <span style={{ color: '#5C9C8C', margin: '5px' }}>Change Password</span>
+          </Link>
         </div>
-        <Link href="/home">
-          <span>Home</span>
-        </Link>
-        <Link href="/account-profile">
-          <span>Account Profile</span>
-        </Link>
-        <Link href="/bank-balance">
-          <span>Bank Balance</span>
-        </Link>
-        <Link href="/change-password">
-          <span>Change Password</span>
-        </Link>
       </div>
     </div>
- );
+  );
 };
 
 export default Navbar;

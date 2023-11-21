9604b2a4c2a2bdaa55e097d529c620688a227bcf
donellnjagix <74359807+donellnjagix@users.noreply.github.com>
2023-11-21 15:42:19 +0300

    Hover on table

--- app/TransactionTable.tsx
+++ app/TransactionTable.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Transaction from './components/types/types';
+
 interface TransactionTableProps {
   transactions: Transaction[];
 }
@@ -9,21 +10,21 @@ const TransactionTable: React.FC<TransactionTableProps> = ({ transactions }) => 
     <table className="table-fixed w-full">
       <thead>
         <tr>
-          <th className="w-1/6">ID</th>
-          <th className="w-2/6">Date</th>
-          <th className="w-1/6">Type</th>
-          <th className="w-1/6">Amount</th>
-          <th className="w-1/6">Status</th>
+          <th className="w-1/6 text-center">ID</th>
+          <th className="w-2/6 text-center">Date</th>
+          <th className="w-1/6 text-center">Type</th>
+          <th className="w-1/6 text-center">Amount</th>
+          <th className="w-1/6 text-center">Status</th>
         </tr>
       </thead>
       <tbody>
         {transactions.map((transaction) => (
-          <tr key={transaction.id}>
-            <td>{transaction.id}</td>
-            <td>{transaction.date.toLocaleString()}</td>
-            <td>{transaction.type}</td>
-            <td>${transaction.amount.toLocaleString('en-US')}</td>
-            <td>{transaction.status}</td>
+          <tr key={transaction.id} className="hover:bg-gray-50 dark:hover:bg-gray-600">
+            <td className="text-center">{transaction.id}</td>
+            <td className="text-center">{transaction.date.toLocaleString()}</td>
+            <td className="text-center">{transaction.type}</td>
+            <td className="text-center">${transaction.amount.toLocaleString('en-US')}</td>
+            <td className="text-center">{transaction.status}</td>
           </tr>
         ))}
       </tbody>
